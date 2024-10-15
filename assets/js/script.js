b59614641a3b53d24f528db241aaf4e368fee497
Becky Williams <beckz14@gmail.com>
2024-10-15 19:17:31 +0000

    setup structure for game function

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer () {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+    
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtrationQuestion () {
+
+}
+
+function displayTimesQuestion () {
+
+}
+
+function displayDivideQuestion () {
+
+}
