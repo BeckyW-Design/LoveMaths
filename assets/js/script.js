de6f7008c16eeb28353aced9345fe87302562fcc
Becky Williams <beckz14@gmail.com>
2024-10-16 15:57:45 +0000

    add code to ensure subtraction game is postive numbers only

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2)
+    } else if (gameType === "subtract") {
+        displaySubtrationQuestion(num1, num2)
     }else
         {
         alert(`Unknown game type: ${gameType}`);
@@ -74,6 +76,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator ==="x") { 
         return [operand1 * operand2, "multiply"];
+    } else if (operator ==="-") { 
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemeneted operator ${operator}`);
         throw `Unimplemeneted operator ${operator}. Aborting!`;
@@ -107,7 +111,11 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtrationQuestion() {
+function displaySubtrationQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
 
 }
 
