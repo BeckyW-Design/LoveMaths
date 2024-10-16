300d31eff1548bb434a4dbc1a73fbdca7a19b4c5
Becky Williams <beckz14@gmail.com>
2024-10-16 11:41:23 +0000

    Add addition questions to game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,56 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button")
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click",function(){
+        button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
-                alert("You Clicked Submit!");
+                alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert (`You clicked ${gameType}`);
+                runGame(gameType);
             }
 
-        })
+        });
     }
-})
 
-function runGame() {
+    runGame("addition");
+});
 
+function runGame(gameType) {
+
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+} else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+}
 }
 
 function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer () {
+function calculateCorrectAnswer() {
 
 }
 
-function incrementScore () {
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer () {
+function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtrationQuestion () {
